b4e25bbc834d25fbcccfde7449dd10a05cbcc412
dongklee42 <easternstar1005@gmail.com>
2021-10-24 15:32:09 +0900

    Update a project, prototype_1

---
+++ Prototype_01/Prototype_01/Controllers/HomeVC.swift
@@ -0,0 +1,13 @@
+//
+//  HomeVC.swift
+//  Prototype_01
+//
+//  Created by 이동규 on 2021/10/24.
+//
+
+import Foundation
+import UIKit
+
+class HomeVC: UIViewController {
+    //
+}
---
+++ Prototype_01/Prototype_01/Controllers/LoginVC.swift
@@ -0,0 +1,13 @@
+//
+//  LoginVC.swift
+//  Prototype_01
+//
+//  Created by 이동규 on 2021/10/24.
+//
+
+import Foundation
+import UIKit
+
+class LoginVC: UIViewController {
+    //
+}
