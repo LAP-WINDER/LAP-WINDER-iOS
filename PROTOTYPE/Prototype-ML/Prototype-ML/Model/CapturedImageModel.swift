1af8d6c3439097cf6984a1c37c1ae2ac9c39d8b0
dongklee42 <easternstar1005@gmail.com>
2021-11-07 23:25:28 +0900

    Update prototype project

---
+++ PROTOTYPE/Prototype-ML/Prototype-ML/Model/CapturedImageModel.swift
@@ -0,0 +1,15 @@
+//
+//  CapturedImageModel.swift
+//  Prototype-ML
+//
+//  Created by 이동규 on 2021/11/03.
+//
+
+import Foundation
+import UIKit
+
+struct CapturedImageModel {
+    var capturedImage: UIImage!
+    var paramName: String!
+    var fileName: String!
+}
